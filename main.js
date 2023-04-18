ff0500012bddf231f73bd4647c6b346e5283a0a9
Eddie Hg <47726314+01Eddie@users.noreply.github.com>
2023-04-18 12:14:48 -0500

    Portafolio de D

---
+++ main.js
@@ -0,0 +1,42 @@
+// Scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+   sections.forEach( sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if (top >= offset && top < offset + height) {
+        navLinks.forEach(links =>{
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+        })
+    }
+   }
+   )
+};
+
+// sticky navbar
+window.onscroll = () =>{
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+// Swiper
+  var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
